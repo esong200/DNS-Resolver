210805392c63a11ee5bcbbb981ce9a2da25e3a2b
Ethan Song <e.song200@gmail.com>
2020-04-29 16:24:53 -0700

    First

---
+++ DNS-Resolver/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DNS-Resolver
+//
+//  Created by Ethan Song on 4/29/20.
+//  Copyright © 2020 Ethan Song. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
